dcebefe0e17d5a64c32f95e0860470f4f3450132
Learner <nilesh.dhongade@apisero.com>
2021-02-12 17:54:23 +0530

    Branch m1 commit, 2nd commit

--- calculator.java
+++ calculator.java
@@ -7,11 +7,19 @@ class MainClass{
 	{
 		return a+b;
 	}
+
+	static int sub(int a,int b)
+	{
+		return a-b;
+	}
+
 	public static void main(String args[])
 	{
 		int a=10;
 		int b=20;
 		System.out.println(" "+sum(a,b));
+
+		System.out.println(" "+sub(a,b));
 		
 	}
 }
