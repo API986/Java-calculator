98cc0174482c3a42a2ad3fea6000b3d2a061549b
API986 <78959791+API986@users.noreply.github.com>
2021-02-17 18:14:56 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+class Test{
+	public static void main(String args[])
+	{
+		
+		System.out.println(" First Line");
+
+		System.out.println(" second Line");
+
+		System.out.println(" Third Line ");
+
+		System.out.println(" Fourth Line");
+
+		System.out.println(" Fifth Line");
+		
+	}
+}
